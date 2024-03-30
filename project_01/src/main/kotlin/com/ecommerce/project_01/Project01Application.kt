a8c2f316d75e64123b5bcd4cffca64c12606f736
faisalmoinuddin99 <faisal25march99@gmail.com>
2024-03-30 11:16:05 +0530

    initial commit

---
+++ project_01/src/main/kotlin/com/ecommerce/project_01/Project01Application.kt
@@ -0,0 +1,11 @@
+package com.ecommerce.project_01
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Project01Application
+
+fun main(args: Array<String>) {
+	runApplication<Project01Application>(*args)
+}
---
+++ project_01/src/test/kotlin/com/ecommerce/project_01/Project01ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ecommerce.project_01
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Project01ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
